b81265d9a163dfbbe8e1ccf36b24df932218e93f
ngominhson <ngo.minh.son@framgia.com>
2015-03-07 02:46:19 +0000

    done reset pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   
+   def index
+    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+  end
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless FILL_IN
   end
 
   def new
@@ -11,9 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)   # Not the final implementation!
     if @user.save
-      log_in @user
-    	flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
